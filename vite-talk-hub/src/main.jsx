0f8257d1d25e1449eb7e0a413b3aa64f5da4b40c
Asad_noob69 <asadaliabbasi787@gmail.com>
2024-07-27 22:38:19 +0500

    vite react

---
+++ vite-talk-hub/src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './ChatApp.jsx'
+import './ChatApp.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <ChatApp />
+  </React.StrictMode>,
+)
